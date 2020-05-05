ebcc5fe2f0a15741790ddbe61962307165df4192
Zac Webb <me@zacwebb.com>
2020-05-05 21:16:56 +1200

    Continued refactoring work

--- main.js
+++ main.js
@@ -1,71 +1,92 @@
 // Necessary to perform any API actions
-var modhash = $("form.logout input[name=uh]").val();
+const modhash = $('form.logout input[name=uh]').val();
 
-function editFlair(thing, flairCss, flairText) {
-    $.post('https://www.reddit.com/r/globaloffensive/api/flair', {
+const editFlair = async (thing, flairCss, flairText) => {
+    const remove = await apiRequest('https://www.reddit.com/r/globaloffensive/api/flair', {
         api_type: 'json',
         css_class: flairCss,
         link: thing,
         text: flairText,
         uh: modhash
-    }).done(function() {
-        var fullID = "#thing" + thing;
-        $(fullID).addClass('linkflair');
     });
-}
 
-function removeWithReason(ev, chromeGet, storage, thing, distinguish, win) {
-    var property = $(ev.target).text().toLowerCase().replace(' ', '');
+    $('#thing' + thing).addClass('linkflair');
+};
+
+const removeWithReason = async (ev, chromeGet, storage, thing, distinguish, ruleClicked, ruleDialog = null) => {
+    const property = $(ev.target).text().toLowerCase().replace(' ', '');
+
+    let removalMessage = $( '#ruleText' ).val();
 
     if (!chromeGet.hasOwnProperty(property)) {
-        chromeGet[property] = "Your thread has been removed.  Please carefully [read our rules](https://www.reddit.com/r/GlobalOffensive/about/rules/) and ask if you have any questions.";
+        chromeGet[property] = 'Your thread has been removed.  Please carefully [read our rules](https://www.reddit.com/r/GlobalOffensive/about/rules/) and ask if you have any questions.';
     }
 
-    var removedThreadLink = 'https://redd.it/' + thing.replace('t3_', '');
-    var removalMessage = $( "#ruleText" ).val();
-    var footer = "";
-
-    if (storage.footer != "") {
-        footerText = storage.footer.replace('%%thread_link%%', removedThreadLink);
-        footer = "\n\n---\n\n" + footerText;
-    } else if (storage.footer === "") {
-        footer = "";
+    if (ruleDialog === null) {
+        removalMessage = chromeGet[ruleClicked];
     }
 
-    var reason = removalMessage + footer;
+    const removedThreadLink = 'https://redd.it/' + thing.replace('t3_', '');
 
-    $.post('https://www.reddit.com/r/globaloffensive/api/remove', {
-        uh: modhash,
-        id: thing,
-        spam: false,
-    }).done(function() {
-        $.post('https://www.reddit.com/r/globaloffensive/api/comment', {
-            parent: thing,
-            text: reason,
+    let footer = '';
+
+    if (storage.footer !== '') {
+        const footerText = storage.footer.replace('%%thread_link%%', removedThreadLink);
+        footer = '\n\n---\n\n' + footerText;
+    } else if (storage.footer === '') {
+        footer = '';
+    }
+
+    const reason = removalMessage + footer;
+
+    try {
+        const remove = await apiRequest('https://www.reddit.com/r/globaloffensive/api/remove', {
             uh: modhash,
-            api_type: 'json'
-        }).done(function(res) {
-            if (distinguish && res.json.hasOwnProperty("errors") && res.json.errors.length == 0) {
-                $.post('https://www.reddit.com/r/globaloffensive/api/distinguish/yes', {
-                    id: res.json.data.things[0].data.id,
-                    uh: modhash,
-                    sticky: true
-                }).done(function() {
-                    if ( win ) {
-                        $( win ).dialog( "close" );
-                    }
-                });
-            }
+            id: thing,
+            spam: 'false',
         });
-    });
 
-    var dd = $(ev.target.parentNode).siblings('.rgo-dropdown');
-    dd.html("removed");
-    dd.removeClass('rgo-dropdown');
-}
+        if (remove.ok) {
+            const comment = await apiRequest('https://www.reddit.com/r/globaloffensive/api/comment', {
+                parent: thing,
+                text: reason,
+                uh: modhash,
+                api_type: 'json'
+            }, true);
 
-function checkNightMode() {
-    function doModeLogic() {
+            const {errors} = comment.json;
+
+            if (errors.length === 0 && distinguish) {
+                const {things} = comment.json.data;
+                await apiRequest('https://www.reddit.com/r/globaloffensive/api/distinguish/yes', {
+                    id: things[0].data.id,
+                    uh: modhash,
+                    sticky: 'true'
+                });
+
+                $(ruleDialog).dialog('close');
+            }
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+    const dropDown = $(ev.target.parentNode).siblings('.rgo-dropdown');
+    dropDown.html('removed');
+    dropDown.removeClass('rgo-dropdown');
+};
+
+const apiRequest = async (url, params, json = false) => {
+    const requestUrl = new URL(url);
+    requestUrl.search = new URLSearchParams(params).toString();
+
+    const response = await fetch(requestUrl.toString(), { method: 'POST' });
+
+    return json ? await response.json() : await response;
+};
+
+const checkNightMode = () => {
+    const doModeLogic = () => {
         if($('body').hasClass('res-nightmode')) {
             // Stop if we've already added the stylesheet
             if($('link#rgo_nightmode').length) { return; }
@@ -79,10 +100,10 @@ function checkNightMode() {
         } else {
             $('link#rgo_nightmode').remove();
         }
-    }
+    };
 
     // hook changes to body's classes to handle night mode changes
-    var observer = new MutationObserver(function(mutations) {
+    const observer = new MutationObserver(() => {
         doModeLogic();
     });
     observer.observe(document.body, {
@@ -92,30 +113,30 @@ function checkNightMode() {
 
     // invoke nightmode logic once to process current state
     doModeLogic();
-}
+};
 
-function addQuickFlair() {
+const addQuickFlair = () => {
     // Add our special button to all flatlists under threads
-    $('.link .flat-list.buttons').each(function (i, e) {
+    $('.link .flat-list.buttons').each((i, e) => {
         // Skip elements we've already added the dropdown to
         if ($(e).has('.rgo-qf-dropdown').length) { return; }
 
         // Create selected option/trigger div
-        var quickFlair = document.createElement('DIV');
+        const quickFlair = document.createElement('DIV');
 
         quickFlair.className = 'dropdown lightdrop rgo-qf-dropdown';
 
-        var qfDrop = document.createElement('SPAN');
+        const qfDrop = document.createElement('SPAN');
         qfDrop.className = 'selected';
         $(qfDrop).text('quick flair');
 
         quickFlair.appendChild(qfDrop);
 
         // Create flair selections div
-        var flairChoices = document.createElement('DIV');
+        const flairChoices = document.createElement('DIV');
         flairChoices.className = 'drop-choices';
 
-        var linkFlairs = {
+        const linkFlairs = {
             'AMA': 'ama',
             'Discussion': 'discussion',
             'Discussion | Esports': 'discussion esports',
@@ -135,8 +156,8 @@ function addQuickFlair() {
             'UGC': 'ugc'
         };
 
-        for (var lf in linkFlairs) {
-            var linkFlair = document.createElement('A');
+        for (const lf in linkFlairs) {
+            const linkFlair = document.createElement('A');
             $(linkFlair).text(lf);
 
             if (lf === 'UGC') {
@@ -146,22 +167,22 @@ function addQuickFlair() {
             }
             linkFlair.className = 'choice';
 
-            $(linkFlair).click(function (ev) {
+            $(linkFlair).click((ev) => {
                 ev.preventDefault();
-                var flairText = ev.target.innerHTML;
-                var flairClicked = linkFlairs[flairText];
+                let flairText = ev.target.innerHTML;
+                const flairClicked = linkFlairs[flairText];
 
                 if (flairText.includes('/')) {
                     flairText = flairText.replace('/', ' & ');
                 }
-                var id = $(ev.target).closest('.thing').attr('data-fullname');
+                const id = $(ev.target).closest('.thing').attr('data-fullname');
 
                 if (flairText === 'UGC') {
                     flairText = 'User Generated Content';
                 }
                 editFlair(id, flairClicked, flairText);
 
-                var dd = $(ev.target.parentNode).siblings('.rgo-qf-dropdown');
+                const dd = $(ev.target.parentNode).siblings('.rgo-qf-dropdown');
                 dd.html('flaired!');
                 dd.removeClass('rgo-qf-dropdown');
             });
@@ -169,8 +190,8 @@ function addQuickFlair() {
             flairChoices.appendChild(linkFlair);
         }
 
-        var li = document.createElement('LI');
-        var spacer = document.createElement('DIV');
+        const li = document.createElement('LI');
+        const spacer = document.createElement('DIV');
         spacer.className = 'spacer';
 
         spacer.appendChild(quickFlair);
@@ -178,11 +199,11 @@ function addQuickFlair() {
         li.appendChild(spacer);
         e.appendChild(li);
     });
-}
+};
 
-function addRemoveWithReasons() {
-    var diaDiv  = document.createElement('DIV'),
-        txtArea = document.createElement('TEXTAREA');
+const addRemoveWithReasons = () => {
+    const diaDiv  = document.createElement('DIV');
+    const txtArea = document.createElement('TEXTAREA');
 
     diaDiv.id  = 'ruleDialog';
     txtArea.id = 'ruleText';
@@ -194,27 +215,27 @@ function addRemoveWithReasons() {
     $('#siteTable').append( diaDiv );
 
     // Add our special button to all flatlists under threads
-    $('.link .flat-list.buttons').each(function(i, e) {
+    $('.link .flat-list.buttons').each((i, e) => {
         // Skip elements we've already added the dropdown to
         if ($(e).has('.rgo-dropdown').length) { return; }
 
         // Create selected option/trigger div
-        var selected = document.createElement('DIV');
+        const selected = document.createElement('DIV');
 
         selected.className = 'dropdown lightdrop rgo-dropdown';
 
-        var selectedSpan = document.createElement('SPAN');
+        const selectedSpan = document.createElement('SPAN');
         selectedSpan.className = 'selected';
         $(selectedSpan).text('remove w/ reason');
 
         selected.appendChild(selectedSpan);
 
         // Create options div
-        var options = document.createElement('DIV');
+        const options = document.createElement('DIV');
         options.className = 'drop-choices';
 
         // The rule by button label and hover-text
-        var rules = {
+        const rules = {
             'Rule 1: Relevancy': 'Relevancy',
             'Rule 2: Quality': 'Quality',
             'Rule 2b: Duplicate': 'Duplicate',
@@ -227,59 +248,61 @@ function addRemoveWithReasons() {
             'Rule 9: Abuse': 'Abuse & Poor Behavior'
         };
 
-        for (var r in rules) {
-            var ruleLink = document.createElement('A');
+        for (const r in rules) {
+            const ruleLink = document.createElement('A');
 
             $(ruleLink).text(r);
             ruleLink.title = rules[r];
             ruleLink.className = 'choice';
 
-            $(ruleLink).click(function(ev) {
+            $(ruleLink).click((ev) => {
                 ev.preventDefault();
-                var ruleClicked = ev.target.innerHTML.replace(' ','').toLowerCase().split(':')[0];
-                var id = $(ev.target).closest('.thing').attr('data-fullname');
+                const id = $(ev.target).closest('.thing').attr('data-fullname');
+                const ruleClicked = ev.target.innerHTML.replace(' ','').toLowerCase().split(':')[0];
 
                 // Default values for the comments for each removal
-                var chromeGet = {
-                    rule1: "Your thread was removed under **[Rule 1](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    rule2: "Your thread was removed under **[Rule 2](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    rule2b: "Your thread was removed as a duplicate under **[Rule 2](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    rule3: "Your thread was removed under **[Rule 3](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    rule4: "Your thread was removed under **[Rule 4](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    rule5: "Your thread was removed under **[Rule 5](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    rule6: "Your thread was removed under **[Rule 6](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    rule7: "Your thread was removed under **[Rule 7](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    rule8: "Your thread was removed under **[Rule 8](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    rule9: "Your thread was removed under **[Rule 9](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.",
-                    footer: "Please take a moment to visit the rule linked above. Many rules contain details which may not be evident by the rule title. If you have any further questions or concerns, please send us a [modmail](https://www.reddit.com/message/compose?to=/r/GlobalOffensive)!",
-                    oneTaps: ""
+                const chromeGet = {
+                    rule1: 'ssssYour thread was removed under **[Rule 1](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    rule2: 'Your thread was removed under **[Rule 2](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    rule2b: 'Your thread was removed as a duplicate under **[Rule 2](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    rule3: 'Your thread was removed under **[Rule 3](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    rule4: 'Your thread was removed under **[Rule 4](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    rule5: 'Your thread was removed under **[Rule 5](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    rule6: 'Your thread was removed under **[Rule 6](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    rule7: 'Your thread was removed under **[Rule 7](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    rule8: 'Your thread was removed under **[Rule 8](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    rule9: 'Your thread was removed under **[Rule 9](https://www.reddit.com/r/GlobalOffensive/about/rules/)**.',
+                    footer: 'Please take a moment to visit the rule linked above. Many rules contain details which may not be evident by the rule title. If you have any further questions or concerns, please send us a [modmail](https://www.reddit.com/message/compose?to=/r/GlobalOffensive)!',
+                    oneTaps: ''
                 };
 
-                browser.storage.local.get(chromeGet, function(storage) {
-                    // If they want one click removals, don't show dialog
-                    if ( storage.oneTaps ) {
-                        removeWithReason(ev, chromeGet, storage, id, true, this);
-                    } else { // Show dialog on each click
-                        var ruleClicked = ev.target.innerHTML.replace(' ','').toLowerCase().split(':')[0];
-                        $("#ruleText").val(chromeGet[ruleClicked]);
-                        $("#ruleText").html(chromeGet[ruleClicked]);
-                        $("#ruleText").css("display", "inline");
+                browser.storage.local.get(chromeGet, (storage) => {
+                    if (storage.oneTaps) {
+                        // If they want one click removals, don't show dialog
+                        removeWithReason(ev, chromeGet, storage, id, true, ruleClicked);
+                    } else {
+                        // Show dialog on each click
+                        const ruleDialog  = '#ruleDialog';
 
-                        $("#ruleDialog").dialog({
+                        $('#ruleText').val(chromeGet[ruleClicked])
+                            .html(chromeGet[ruleClicked])
+                            .css('display', 'inline');
+
+                        $(ruleDialog).dialog({
                             height  : 200,
                             width   : 700,
                             modal   : true,
-                            title   : ev.target.innerHTML + " Removal",
+                            title   : ev.target.innerHTML + ' Removal',
                             buttons : {
-                                "Remove and lock comment": function() {
+                                'Remove and lock comment': () => {
                                     // TODO
                                 },
-                                "Get Salt": function() {
+                                'Get Salt': () => {
                                     // This actually makes the POSTs happen and removes the thread, it also closes the dialog on completion
-                                    removeWithReason(ev, chromeGet, storage, id, true, this);
+                                    removeWithReason(ev, chromeGet, storage, id, true, ruleClicked, ruleDialog);
                                 },
-                                "Abort!" : function() {
-                                    $( this ).dialog( "close" );
+                                'Abort!' : () => {
+                                    $(ruleDialog).dialog('close');
                                 }
                             }
                         });
@@ -288,24 +311,23 @@ function addRemoveWithReasons() {
             });
             options.appendChild(ruleLink);
         }
-        var li = document.createElement('LI');
-        var spacer = document.createElement('DIV');
-        spacer.className = "spacer";
+        const li = document.createElement('LI');
+        const spacer = document.createElement('DIV');
+        spacer.className = 'spacer';
 
         spacer.appendChild(selected);
         spacer.appendChild(options);
         li.appendChild(spacer);
         e.appendChild(li);
     });
-}
+};
 
 // Hook our functions
-$(window).on('neverEndingLoad', function () {
+$(window).on('neverEndingLoad', () => {
     addQuickFlair();
     addRemoveWithReasons();
 });
-$(document).ready(function () {
-    checkNightMode();
-    addQuickFlair();
-    addRemoveWithReasons();
-});
+
+checkNightMode();
+addQuickFlair();
+addRemoveWithReasons();
